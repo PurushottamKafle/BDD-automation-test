26a9b132e42f8feff723adb950bbdcb8b83fa192
Purushottam Kafle <32263675+PurushottamKafle@users.noreply.github.com>
2018-05-29 15:40:23 +0545

    Update App.java

--- src/main/java/Cucumberproject/App.java
+++ src/main/java/Cucumberproject/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       printMe("Biplap");
+    }
+    public static void printMe(String x) {
+        String y = x;
+        System.out.println("Hello World! by "+y);
     }
 }
